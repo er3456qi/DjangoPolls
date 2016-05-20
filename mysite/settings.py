c537ab3276a7b6fdc2d6adc004acb24bb0b24a8a
er3456qi <er3456qi@outlook.com>
2016-05-20 21:42:56 +0800

    part2 add models

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'polls.apps.PollsConfig',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    """
+    Each field is represented by an instance of a Field class
+    – e.g., CharField for character fields and DateTimeField for datetime.
+    This tells Django what type of data each field holds.
+    The name of each Field instance (e.g. question_text or pub_date) is the field’s name
+
+    You can use an optional first positional argument to a Field to designate a human-readable name.
+    That’s used in a couple of introspective parts of Django, and it doubles as documentation.
+    If this field isn’t provided, Django will use the machine-readable name.
+    In this example, we’ve only defined a human-readable name for Question.pub_date.
+    """
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
