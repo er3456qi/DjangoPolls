e20f9bcb985c043522e23e74b2993ea33d3b9dce
er3456qi <er3456qi@outlook.com>
2016-05-20 21:44:41 +0800

    part2 add models fix bug

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -47,7 +48,6 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'polls.apps.PollsConfig',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
