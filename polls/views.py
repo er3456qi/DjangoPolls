2cf0e6d534cd68b9161d68ec156e60526725967f
er3456qi <er3456qi@outlook.com>
2016-05-21 08:30:43 +0800

    part4 use generic views

--- polls/views.py
+++ polls/views.py
@@ -1,28 +1,59 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 # Create your views here.
 
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+# use generic view replaced
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return render(request, 'polls/index.html', context)
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class IndexView(generic.ListView):
+    """
+     The template_name attribute is used to tell Django.
+     to use a specific template name instead of the auto generated default template name.
+     because by default, generic.ListView use a template called '<app name>/<model name>_list.html'
+    """
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+
+class DetailView(generic.DetailView):
+    """
+        For DetailView the question variable is provided automatically – since we’re using a Django model (Question),
+        Django is able to determine an appropriate name for the context variable.
+        However, for ListView, the automatically generated context variable is question_list.
+        To override this we provide the context_object_name attribute,
+        specifying that we want to use latest_question_list instead.
+    """
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
