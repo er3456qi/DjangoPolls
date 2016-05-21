b15a7d90516250ea4e5edd1cb70345877a639758
er3456qi <er3456qi@outlook.com>
2016-05-21 08:19:47 +0800

    part4 update views and add template

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -19,9 +21,29 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You did not select a choice.',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing with POST data. This
+        # This prevents data from being posted twice if a user hits the Back button.
+        # reverse() helps avoid having to hardcode a URL in the view function.
+        # It is given the name of the view that we want to pass control to and
+        # the variable portion of the URL pattern that points to that view.
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+
+
