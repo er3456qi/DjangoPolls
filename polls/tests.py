beb5b58f60f9a237a6ba76a430cac84243b53efd
er3456qi <er3456qi@outlook.com>
2016-05-21 08:48:55 +0800

    part5 add more comprehensive tests

--- polls/tests.py
+++ polls/tests.py
@@ -18,3 +18,22 @@ class QuestionMethodTests(TestCase):
         future_question = Question(pub_date=time)
         self.assertFalse(future_question.was_published_recently())
 
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for questions
+        whose pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertFalse(old_question.was_published_recently())
+
+    def test_was_published_rectendly_with_rectent_question(self):
+        """
+        was_published_recently() should return True for questions
+        whose pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertTrue(recent_question.was_published_recently())
+
+
